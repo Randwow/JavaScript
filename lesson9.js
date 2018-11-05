d2920949c43614c39c86f6ce779ff681aa09d5e3
Randwow <makeev.n.00@mail.ru>
2018-11-05 21:07:45 +0200

    SU-009 Home work

--- lesson9.js
+++ lesson9.js
@@ -13,4 +13,4 @@ console.log(typeof (f));
 var myCurrentAge = 18;
 if (myCurrentAge >= 25) { console.log('I am over 25 years old') }
 else { console.log('I am under 25') }
-myCurrentAge >= 25 ? console.log("I am over 25 years old") : console.log * ("I am under 25");
+myCurrentAge >= 25 ? console.log("I am over 25 years old") : console.log("I am under 25");
