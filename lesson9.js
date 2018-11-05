ef26b80146c373f9b2b6b62a64d1f2a7be11d8b9
Randwow <makeev.n.00@mail.ru>
2018-11-05 21:06:10 +0200

    SU-009 Home work

--- lesson9.js
+++ lesson9.js
@@ -1,16 +1,16 @@
-    var a = 1;
-    var b = true;
-    var c = 'JavaScript';
-    var d = null;
-    var e = { name: "Петя" };
-    var f;
-        typeof (a);
-        typeof (b);
-        typeof (c);
-        typeof (d);
-        typeof (e);
-        typeof (f);
-    var myCurrentAge = 18;
-        if (myCurrentAge >= 25) { console.log('I am over 25 years old') }
-        else { console.log('I am under 25') }
-    myCurrentAge >= 25 ? "I am over 25 years old" : "I am under 25";
+var a = 1;
+var b = true;
+var c = 'JavaScript';
+var d = null;
+var e = { name: "Петя" };
+var f;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+console.log(typeof (d));
+console.log(typeof (e));
+console.log(typeof (f));
+var myCurrentAge = 18;
+if (myCurrentAge >= 25) { console.log('I am over 25 years old') }
+else { console.log('I am under 25') }
+myCurrentAge >= 25 ? console.log("I am over 25 years old") : console.log * ("I am under 25");
